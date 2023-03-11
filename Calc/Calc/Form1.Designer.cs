97f05e1d0b71bfa2a5d60ba060f884523aa2ebf8
Abhiratn <83910553+Abhiratn@users.noreply.github.com>
2023-03-11 18:07:16 +0530

    Add files via upload

---
+++ Calc/Calc/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_n1 = new System.Windows.Forms.Label();
+            this.lbl_n2 = new System.Windows.Forms.Label();
+            this.lbl_op = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.btn_mul = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.lbl_res = new System.Windows.Forms.Label();
+            this.btn_clr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_n1
+            // 
+            this.lbl_n1.AutoSize = true;
+            this.lbl_n1.Location = new System.Drawing.Point(13, 31);
+            this.lbl_n1.Name = "lbl_n1";
+            this.lbl_n1.Size = new System.Drawing.Size(38, 13);
+            this.lbl_n1.TabIndex = 0;
+            this.lbl_n1.Text = "Num 1";
+            // 
+            // lbl_n2
+            // 
+            this.lbl_n2.AutoSize = true;
+            this.lbl_n2.Location = new System.Drawing.Point(13, 109);
+            this.lbl_n2.Name = "lbl_n2";
+            this.lbl_n2.Size = new System.Drawing.Size(38, 13);
+            this.lbl_n2.TabIndex = 1;
+            this.lbl_n2.Text = "Num 2";
+            // 
+            // lbl_op
+            // 
+            this.lbl_op.AutoSize = true;
+            this.lbl_op.Location = new System.Drawing.Point(118, 66);
+            this.lbl_op.Name = "lbl_op";
+            this.lbl_op.Size = new System.Drawing.Size(0, 13);
+            this.lbl_op.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Location = new System.Drawing.Point(16, 177);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(75, 23);
+            this.btn_plus.TabIndex = 5;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.Location = new System.Drawing.Point(111, 176);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(75, 23);
+            this.btn_sub.TabIndex = 6;
+            this.btn_sub.Text = "-";
+            this.btn_sub.UseVisualStyleBackColor = true;
+            this.btn_sub.Click += new System.EventHandler(this.btn_sub_Click);
+            // 
+            // btn_mul
+            // 
+            this.btn_mul.Location = new System.Drawing.Point(16, 231);
+            this.btn_mul.Name = "btn_mul";
+            this.btn_mul.Size = new System.Drawing.Size(75, 23);
+            this.btn_mul.TabIndex = 7;
+            this.btn_mul.Text = "X";
+            this.btn_mul.UseVisualStyleBackColor = true;
+            this.btn_mul.Click += new System.EventHandler(this.btn_mul_Click);
+            // 
+            // btn_div
+            // 
+            this.btn_div.Location = new System.Drawing.Point(111, 231);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(75, 23);
+            this.btn_div.TabIndex = 8;
+            this.btn_div.Text = "/";
+            this.btn_div.UseVisualStyleBackColor = true;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // lbl_res
+            // 
+            this.lbl_res.AutoSize = true;
+            this.lbl_res.Location = new System.Drawing.Point(108, 139);
+            this.lbl_res.Name = "lbl_res";
+            this.lbl_res.Size = new System.Drawing.Size(0, 13);
+            this.lbl_res.TabIndex = 9;
+            // 
+            // btn_clr
+            // 
+            this.btn_clr.Location = new System.Drawing.Point(64, 260);
+            this.btn_clr.Name = "btn_clr";
+            this.btn_clr.Size = new System.Drawing.Size(75, 23);
+            this.btn_clr.TabIndex = 10;
+            this.btn_clr.Text = "Clear";
+            this.btn_clr.UseVisualStyleBackColor = true;
+            this.btn_clr.Click += new System.EventHandler(this.btn_clr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 324);
+            this.Controls.Add(this.btn_clr);
+            this.Controls.Add(this.lbl_res);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.btn_mul);
+            this.Controls.Add(this.btn_sub);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_op);
+            this.Controls.Add(this.lbl_n2);
+            this.Controls.Add(this.lbl_n1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_n1;
+        private System.Windows.Forms.Label lbl_n2;
+        private System.Windows.Forms.Label lbl_op;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.Button btn_mul;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.Label lbl_res;
+        private System.Windows.Forms.Button btn_clr;
+    }
+}
+
---
+++ Calc/Calc/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+            lbl_op.Text = "+";
+            double n1 = double.Parse(textBox1.Text);
+            double n2 = double.Parse(textBox2.Text);
+            double result = n1+n2;
+            lbl_res.Text = Convert.ToString(result);
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            lbl_op.Text = "-";
+            double n1 = double.Parse(textBox1.Text);
+            double n2 = double.Parse(textBox2.Text);
+            double result = n1 - n2;
+            lbl_res.Text = Convert.ToString(result);
+        }
+
+        private void btn_mul_Click(object sender, EventArgs e)
+        {
+            lbl_op.Text = "X";
+            double n1 = double.Parse(textBox1.Text);
+            double n2 = double.Parse(textBox2.Text);
+            double result = n1 * n2;
+            lbl_res.Text = Convert.ToString(result);
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbl_op.Text = "/";
+                double n1 = double.Parse(textBox1.Text);
+                double n2 = double.Parse(textBox2.Text);
+                if(n2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                double result = n1 / n2;
+                lbl_res.Text = Convert.ToString(result);
+            }
+            catch (DivideByZeroException)
+            {
+                lbl_res.Text = "Cant divide by Zero";
+            }
+
+
+        }
+
+        private void btn_clr_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            lbl_op.Text = "";
+            lbl_res.Text = "";
+        }
+    }
+}
